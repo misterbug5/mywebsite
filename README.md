950ddbda3a298258686593bff3af2fb9eec0e4b8
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2021-09-29 12:32:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Website
+
+This will be my website using spring boot.
+
+The main idea is make a frontend in Angular that takes resources via AJAX to two backends. One in Django and the other in spring. This is the spring backend
---
+++ src/test/java/io/github/misterbug5/mywebsite/MywebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.misterbug5.mywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
